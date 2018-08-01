85dab11680b16071cc11ef7f2e5d5f95409d8eef
Evan Brandt <evanmbrandt@gmail.com>
2018-08-01 08:47:53 -0400

    adding servo code

---
+++ servo.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(3, GPIO.OUT)
+
+pwm = GPIO.PWM(3, 50)
+pwm.start(0)
+
+def setAngle(angle):
+	duty = angle / 18 + 2
+	GPIO.output(3, True)
+	pwm.ChangeDutyCycle(duty)
+	sleep(1)
+	GPIO.output(3, False)
+	pwm.ChangeDutyCycle(0)
+
+setAngle(90)
+sleep(1)
+setAngle(45)
+sleep(1)
+setAngle(20)
+
+pwm.stop()
+GPIO.cleanup()
